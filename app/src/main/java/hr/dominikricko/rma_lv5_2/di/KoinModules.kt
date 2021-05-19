c65d03f779e547373713d0a60a76a3907a0e7a5c
Dominik Ričko <dominik.ricko@gmail.com>
2021-05-19 14:44:50 +0200

    added viewModel and Koin

---
+++ app/src/main/java/hr/dominikricko/rma_lv5_2/di/KoinModules.kt
@@ -0,0 +1,9 @@
+package hr.dominikricko.rma_lv5_2.di
+
+import hr.dominikricko.rma_lv5_2.ui.viewmodel.ActivityViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModel = module{
+    viewModel { ActivityViewModel() }
+}
---
+++ app/src/main/java/hr/dominikricko/rma_lv5_2/ui/viewmodel/ActivityViewModel.kt
@@ -0,0 +1,13 @@
+package hr.dominikricko.rma_lv5_2.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ActivityViewModel : ViewModel(){
+
+    var latitude : String = "0.00000"
+    var longitude : String = "0.00000"
+    var address : String = "unknown"
+    var state : String = "unknown"
+    var place : String = "unknown"
+
+}
