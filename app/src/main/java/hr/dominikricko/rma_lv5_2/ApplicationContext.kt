4a571a89cc7f8b5f53d0d3b637597917963f5620
Dominik Ričko <dominik.ricko@gmail.com>
2021-05-19 14:36:29 +0200

    added dataBinding

--- app/src/main/java/hr/dominikricko/rma_lv5_2/ApplicationContext.kt
+++ app/src/main/java/hr/dominikricko/rma_lv5_2/ApplicationContext.kt
@@ -2,6 +2,9 @@ package hr.dominikricko.rma_lv5_2
 
 import android.app.Application
 import android.content.Context
+import hr.dominikricko.rma_lv5_2.di.viewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
 
 class ApplicationContext: Application() {
 
@@ -15,6 +18,11 @@ class ApplicationContext: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        startKoin{
+            androidContext(this@ApplicationContext)
+            modules(viewModel)
+        }
     }
 
 }
--- app/src/main/java/hr/dominikricko/rma_lv5_2/ui/activity/MainActivity.kt
+++ app/src/main/java/hr/dominikricko/rma_lv5_2/ui/activity/MainActivity.kt
@@ -1,16 +1,31 @@
-package hr.dominikricko.rma_lv5_2.ui
+package hr.dominikricko.rma_lv5_2.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import hr.dominikricko.rma_lv5_2.R
+import hr.dominikricko.rma_lv5_2.databinding.ActivityMainBinding
+import hr.dominikricko.rma_lv5_2.ui.viewmodel.ActivityViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import pub.devrel.easypermissions.EasyPermissions
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel by viewModel<ActivityViewModel>()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        binding.also{
+
+        }
+
     }
 
     override fun onRequestPermissionsResult(
